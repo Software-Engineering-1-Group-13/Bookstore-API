380ea490c9c790ba983c662557625f326a87d40a
Anthony Irizarry <142277677+airiz04@users.noreply.github.com>
2023-09-25 21:00:12 -0400

    Add files via upload

---
+++ src/main/java/com/bookstore/bookstoreapi/data/entity/Rating.java
@@ -0,0 +1,41 @@
+package com.bookstore.bookstoreapi.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Rating")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "RatingID")
+    private Long id;
+
+    @Column(name = "Book")
+    private Book book;
+
+    @Column(name = "Stars")
+    private Integer stars;
+
+
+}
