44a0c9f64b9ee38443c1ff8dbd9f4cfa677174ab
Daniel Ibarra <dannibar201@gmail.com>
2023-10-23 18:28:01 -0400

    Fixed wild card problem

--- src/main/java/com/bookstore/bookstoreapi/data/entity/Customer.java
+++ src/main/java/com/bookstore/bookstoreapi/data/entity/Customer.java
@@ -1,8 +1,18 @@
 package com.bookstore.bookstoreapi.data.entity;
 
-import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
