6a2a75e6d2ffa2830b822bb3bf7443f8ca7f9fee
rheaden10 <rjack070@fiu.edu>
2023-10-28 20:25:21 -0400

    Update to BookController.java

--- src/main/java/com/bookstore/bookstoreapi/data/controller/BookController.java
+++ src/main/java/com/bookstore/bookstoreapi/data/controller/BookController.java
@@ -6,14 +6,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "/books")
 @RestController
 public class BookController {
 
   @Autowired private BookService bookService;
 
-  @PostMapping("/books/create")
+  @PostMapping("/create")
   public ResponseEntity<Book> createABook(@RequestBody Book book) {
     Optional<Book> createdBook = bookService.createABook(book);
     if (createdBook.isEmpty()) {
@@ -22,7 +28,7 @@ public class BookController {
     return new ResponseEntity<>(createdBook.get(), HttpStatus.CREATED);
   }
 
-  @GetMapping("/books/{ISBN}")
+  @GetMapping("/{ISBN}")
   public ResponseEntity<Book> findByISBN(@PathVariable String ISBN) {
     Book book = bookService.findByISBN(ISBN);
 
