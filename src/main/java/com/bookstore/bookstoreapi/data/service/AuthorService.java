e6617b13daa38f1c9176365b07794ded4fde38c4
rheaden10 <rjack070@fiu.edu>
2023-10-30 14:22:23 -0400

    Create an author with first name, last name, biography, and publisher.

---
+++ src/main/java/com/bookstore/bookstoreapi/data/service/AuthorService.java
@@ -0,0 +1,18 @@
+package com.bookstore.bookstoreapi.data.service;
+
+import com.bookstore.bookstoreapi.data.entity.Author;
+import com.bookstore.bookstoreapi.data.repository.AuthorRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+  @Autowired private AuthorRepository authorRepository;
+
+  public Optional<Author> createAuthor(Author author) {
+    Author createdAuthor = authorRepository.save(author);
+    return Optional.of(createdAuthor);
+  }
+}
