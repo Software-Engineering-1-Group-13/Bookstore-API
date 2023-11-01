5906c839e47ab84dcc40b47ad7b68f11c334df6b
airiz04 <airiz04@outlook.com>
2023-10-31 23:40:22 -0400

    Applied spotlessApply

--- src/main/java/com/bookstore/bookstoreapi/data/controller/BookController.java
+++ src/main/java/com/bookstore/bookstoreapi/data/controller/BookController.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
